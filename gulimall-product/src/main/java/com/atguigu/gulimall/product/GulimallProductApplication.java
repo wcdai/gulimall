f985ac015b960ffdda8905042f48d47d64770369
wcdai <1319105206@qq.com>
2021-10-11 21:32:17 +0800

    完成需求1

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @author daiwencheng
+ */
 @MapperScan("com.atguigu.gulimall.product.dao")
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages="com.atguigu.gulimall.product.feign")
